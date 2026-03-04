1ad87d74aa15c1da03dadce28332b4df20d63b6b
LuisHZapata <zapataluish@gmail.com>
2026-03-03 19:37:03 -0500

    Form contact us fixed

--- src/app/domains/info/pages/contact/contact.component.ts
+++ src/app/domains/info/pages/contact/contact.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  private route = inject(ActivatedRoute);
+
+  success = false;
+
+  constructor() {
+    this.route.queryParams.subscribe(params => {
+      if (params['success'] === 'true') {
+        this.success = true;
+        setTimeout(() => this.success = false, 5000);
+      }
+    });
+  }
 }
